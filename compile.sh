4660132cbdc15aab30d252440340866ea785974e
Michael Wisely <michaelwisely@gmail.com>
2014-12-12 13:04:54 -0600

    Check command line arguments to compile.sh

--- compile.sh
+++ compile.sh
@@ -6,4 +6,16 @@ then
     exit 1
 fi
 
+if [ "$#" -lt 1 ]
+then
+    echo "Please provide a MIPL file to compile"
+    exit 1
+fi
+
+if [ ! -e "$1" ]
+then
+    echo "Please provide a MIPL file to compile"
+    exit 1
+fi
+
 ./IRGen $1 2>&1 | llc-3.4 | gcc -x assembler ${*:2} -
